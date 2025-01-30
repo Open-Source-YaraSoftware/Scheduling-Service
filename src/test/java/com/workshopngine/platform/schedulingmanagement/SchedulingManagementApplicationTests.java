057fe8b1f9ee55c8a6584fe0de63c8ebde6c6480
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-01-29 21:30:43 -0500

    chore: set basic test

---
+++ src/test/java/com/workshopngine/platform/schedulingmanagement/SchedulingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshopngine.platform.schedulingmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
