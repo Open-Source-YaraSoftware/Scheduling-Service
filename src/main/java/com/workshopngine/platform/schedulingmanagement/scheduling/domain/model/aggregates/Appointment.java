6da7c87810079598b766d436417fdc182a509379
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-01-29 21:32:11 -0500

    feat: add appointment aggregate entity

---
+++ src/main/java/com/workshopngine/platform/schedulingmanagement/scheduling/domain/model/aggregates/Appointment.java
@@ -0,0 +1,69 @@
+package com.workshopngine.platform.schedulingmanagement.scheduling.domain.model.aggregates;
+
+import com.workshopngine.platform.schedulingmanagement.scheduling.domain.model.commands.CreateAppointmentCommand;
+import com.workshopngine.platform.schedulingmanagement.scheduling.domain.model.valueobjects.DateTimeRange;
+import com.workshopngine.platform.schedulingmanagement.scheduling.domain.model.valueobjects.*;
+import com.workshopngine.platform.schedulingmanagement.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Duration;
+
+@Getter
+@Setter
+@Entity
+public class Appointment extends AuditableAbstractAggregateRoot<Appointment> {
+    @Embedded
+    private ClientId clientId;
+
+    @Embedded
+    private VehicleId vehicleId;
+
+    @Embedded
+    private MechanicId mechanicId;
+
+    @Embedded
+    private WorkshopId workshopId;
+
+    @Enumerated(EnumType.STRING)
+    private EServiceType serviceType;
+
+    @Embedded
+    private DateTimeRange timeRange;
+
+    @Enumerated(EnumType.STRING)
+    private EAppointmentStatus status;
+
+    @Embedded
+    private ServiceDuration duration;
+
+    public Appointment() {
+        super();
+        this.status = EAppointmentStatus.SCHEDULED;
+        this.duration = new ServiceDuration();
+    }
+
+    public Appointment(CreateAppointmentCommand command){
+        this();
+        this.clientId = command.clientId();
+        this.vehicleId = command.vehicleId();
+        this.mechanicId = command.mechanicId();
+        this.workshopId = command.workshopId();
+        this.serviceType = command.serviceType();
+        this.timeRange = command.dateTimeRange();
+        verifyServiceDuration();
+    }
+
+    private void verifyServiceDuration(){
+        if (this.serviceType.equals(EServiceType.DIAGNOSTIC)){
+            this.duration.setEstimatedDuration(Duration.ofHours(1));
+        }
+        if (this.serviceType.equals(EServiceType.MAINTENANCE)){
+            this.duration.setEstimatedDuration(Duration.ofHours(1).plus(Duration.ofMinutes(30)));
+        }
+        if (this.serviceType.equals(EServiceType.REPAIR)){
+            this.duration.setEstimatedDuration(Duration.ofHours(2));
+        }
+    }
+}
