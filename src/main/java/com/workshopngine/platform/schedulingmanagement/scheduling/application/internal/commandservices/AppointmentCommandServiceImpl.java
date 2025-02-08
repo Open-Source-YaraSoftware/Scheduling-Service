5d99a3a7c87ce49347d173a76b9b71f9cc5663ac
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-08 00:33:13 -0500

    feat: use external services to check availability of workshop and mechanic
    Check that there is no conflicts with other appointments

--- src/main/java/com/workshopngine/platform/schedulingmanagement/scheduling/application/internal/commandservices/AppointmentCommandServiceImpl.java
+++ src/main/java/com/workshopngine/platform/schedulingmanagement/scheduling/application/internal/commandservices/AppointmentCommandServiceImpl.java
@@ -1,5 +1,6 @@
 package com.workshopngine.platform.schedulingmanagement.scheduling.application.internal.commandservices;
 
+import com.workshopngine.platform.schedulingmanagement.scheduling.application.internal.outboundservices.acl.StaffManagementContextFacade;
 import com.workshopngine.platform.schedulingmanagement.scheduling.application.internal.outboundservices.acl.WorkshopManagementContextFacade;
 import com.workshopngine.platform.schedulingmanagement.scheduling.domain.model.aggregates.Appointment;
 import com.workshopngine.platform.schedulingmanagement.scheduling.domain.model.commands.CreateAppointmentCommand;
@@ -13,10 +14,19 @@ import org.springframework.stereotype.Service;
 public class AppointmentCommandServiceImpl implements AppointmentCommandService {
     private final AppointmentRepository appointmentRepository;
     private final WorkshopManagementContextFacade workshopManagementContextFacade;
+    private final StaffManagementContextFacade staffManagementContextFacade;
 
     @Override
     public String handle(CreateAppointmentCommand command) {
         var newAppointment = new Appointment(command);
+        var isWorkshopAvailable = this.workshopManagementContextFacade.checkWorkshopAvailability(command.workshopId().workshopId(), command.dateTimeRange().getStartedAt());
+        if (!isWorkshopAvailable.available()) throw new RuntimeException("Workshop is not available");
+        var isStaffAvailable = this.staffManagementContextFacade.checkMechanicAvailability(command.mechanicId().mechanicId(), command.dateTimeRange().getStartedAt());
+        if (!isStaffAvailable.available()) throw new RuntimeException("Mechanic is not available");
+        if (this.appointmentRepository
+                .existsByTimeRange_StartedAtLessThanEqualAndTimeRange_EndedAtGreaterThanEqual(newAppointment.getTimeRange().getStartedAt(), newAppointment.getTimeRange().getEndedAt())
+        ) throw new RuntimeException("Requested time is not available");
+
         try {
             this.appointmentRepository.save(newAppointment);
             newAppointment.sendAppointmentCreatedEvent();
